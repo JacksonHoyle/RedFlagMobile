3b6072e0f4b836d63c295a818851747f8d13c3f7
t364x544 <49071683+t364x544@users.noreply.github.com>
2021-02-17 13:15:51 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AppContainer from './navigation/AppNavigator'
+//I put AppNavigator in a folder within the main project directory called navigation so if you have a different file layout, make sure to change the location in the line of code directly above this comment
 
 export default function App() {
 	return <AppContainer />
