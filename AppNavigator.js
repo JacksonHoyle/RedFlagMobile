c988e64b64d464bc886d8f651f0e85698ec3aa1a
t364x544 <49071683+t364x544@users.noreply.github.com>
2021-02-17 13:02:59 -0600

    Update AppNavigator.js

--- AppNavigator.js
+++ AppNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator} from 'react-navigation-stack'
 import {createAppContainer} from 'react-navigation'
 import Home from '../screens/Home'
 import Browser from '../screens/Browser'
+//I put Home and Browser in a folder within the main project directory called screens because they are different screens the user accesses so if you have a different file layout, make sure to change their location in the two lines of code directly above this comment
 
 const HomeStack = createStackNavigator({
 	Home: {
